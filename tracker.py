3524bef1461288f83266c56b2db5bef33a6d4aa9
Myseolumic <karl32123@gmail.com>
2019-09-10 17:19:20 +0300

    v1.5, a single hedge should display, untested

--- tracker.py
+++ tracker.py
@@ -6,30 +6,53 @@ import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+#whole app requires: pyserial, crcmod, matplotlib
 
-def main():
+class TopMenu:
+
+    def __init__(self, root):
+        menu = Menu(root)
+        root.config(menu=menu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Place Beacon", command=self.place_beacon)
+        editMenu.add_command(label="Add room plan", command=self.place_beacon)
+        menu.add_cascade(label="Edit", menu=editMenu)
+
+        menu.add_command(label="Review", command=self.place_beacon())
+
+
+    def place_beacon(self):
+        print("placed.")
+
+class BigCanvas:
+
+    def __init__(self, root):
+        self.canvas = Canvas(root, width=200, height=200)
+        self.canvas.grid(row=2, column=0, columnspan=3, sticky='nsew')
+        self.beacon = self.canvas.create_rectangle(0,0,20,20)
+
+    def update_beacon(self, x, y):
+        self.canvas.coords(self.beacon, x, y)
+
+
+def main(window, canvas):
     hedge = MarvelmindHedge(tty="\\.\COM4", adr=None, debug=False)  # create MarvelmindHedge thread
     hedge.start()  # start thread
-    f = open("demofile2.txt", "a")
     while True:
         try:
-            # print (hedge.position()) # get last position and print
             sleep(1)
             hedge.print_position()
             if (hedge.distancesUpdated):
                 hedge.print_distances()
 
-            print(hedge.position() )
-            #print("Raw data:")
-            #print("valuesUltrasoundPosition: " + str(hedge.valuesUltrasoundPosition))
-            #print("valuesImuData: " + str(hedge.valuesImuData))
-            #print("valuesImuRawData: " + str(hedge.valuesImuRawData))
-            #print()
-            #window.update()
-            #f.write(hedge.position()+';'+hedge.distances())
+            print(hedge.position())
+            coords = hedge.position()
+            canvas.update_beacon(coords[1], coords[2])
+            window.update()
+
         except KeyboardInterrupt:
             hedge.stop()  # stop and close serial port
-            f.close()
             sys.exit()
 
 
@@ -39,23 +62,11 @@ for i in range(1,6):
     x.append(i)
     y.append(i*2)
 
-#window = Tk()
-#window.geometry("300x300")
-#window.title("Coopi tracker")
+window = Tk()
+window.geometry("300x300")
+window.title("Coopi tracker")
 
-main()
+TopMenu(window)
+canvas = BigCanvas(window)
 
-#figure = Figure(figsize=(4, 4), dpi=70)
-#figure.suptitle('Location')
-
-#a = figure.add_subplot(111)
-#a.plot(x,y,marker='o')
-#a.grid()
-
-#canvas = FigureCanvasTkAgg(figure, master=window)
-#canvas.draw()
-
-#graph_widget = canvas.get_tk_widget()
-#graph_widget.grid(row=0, column=0, columnspan=3, sticky='nsew')
-
-
+main(window, canvas)
