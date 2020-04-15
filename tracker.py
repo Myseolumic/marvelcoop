4e929e75f3866490d3e9d62e302c5ef0e61b276e
Myseolumic <karl32123@gmail.com>
2020-04-16 00:19:57 +0300

    implemented panning #2 readded the menu bar

--- tracker.py
+++ tracker.py
@@ -27,6 +27,9 @@ class SampleApp(Tk):
         self._frame = new_frame
         self._frame.pack()
 
+        if frame_class == ReplayFrame:
+            self._frame.canvas_widget.menubar(self)
+
 
 class BeaconPath(object):
     def __init__(self, id, mat):
@@ -76,6 +79,7 @@ class CommThread(th.Thread):
 class ReplayFrame(Frame):
 
     def __init__(self, root):
+        # Init objects
         Frame.__init__(self, root)
         self.canvas_widget = CanvasWidget(self, 0, 1)
         self.file = FileDialog.askopenfile(parent=root, mode='r', title='Choose a file')
@@ -185,15 +189,59 @@ class CanvasWidget:
         self.canvas.grid(row=row, column=column, columnspan=3, rowspan=3, sticky='nsew')
         self.beacon = self.canvas.create_rectangle(0, 0, 20, 20, fill="blue")
         self.beacon = self.canvas.create_rectangle(0, 0, 20, 20, fill="red")
-        self.canvas.bind('<Button-1>', self.place_beacon)
+        self.canvas.bind('<Button-1>', self.handle_mouse_click)
+        #self.canvas.bind('<MouseWheel>', self.handle_mouse_wheel)
+        self.canvas.bind('<B1-Motion>', self.pan_canvas)
+        self.canvas.bind('<ButtonRelease-1>', self.end_pan)
         self.cs_x = 0
         self.cs_y = 0
         self.zoom = 30
+
+        self.PLACING_BEACON = False
+        self.PLACING_IMAGE = False
+        self.PANNING = False
+
+    def handle_mouse_wheel(self, event):
+        """ Zoom with mouse wheel """
+        x = self.canvas.canvasx(event.x)  # get coordinates of the event on the canvas
+        y = self.canvas.canvasy(event.y)
+
+        scale = 1.0
+        if event.delta == -120:  # scroll down
+            print("scrolling down")
+        if event.delta == 120:  # scroll up, bigger
+            print("scrolling up")
+
+        #self.canvas.scale('all', x, y, scale, scale)  # rescale all objects
+
+    def handle_mouse_click(self, event):
+        print(self.PLACING_BEACON)
+        if self.PLACING_BEACON:
+            self.place_beacon(event)
+            self.PLACING_BEACON = False
+        else:
+            self.canvas.scan_mark(event.x, event.y)
+            self.PANNING = True
+
+    def pan_canvas(self, event):
+        if self.PANNING:
+            self.canvas.scan_dragto(event.x, event.y, gain=1)
+            self.canvas.scan_mark(event.x, event.y)
+
+    def end_pan(self, event):
+        if self.PANNING:
+            self.PANNING = False
 
     def place_beacon(self, event):
         self.cs_x = event.x
         self.cs_y = event.y
         self.draw_origin()
+
+    def add_beacon(self):
+        self.PLACING_BEACON = True
+
+    def add_floor_plan(self):
+        self.PLACING_IMAGE = True
 
     def draw_origin(self):
         self.canvas.create_oval(self.cs_x - 5, self.cs_y - 5, self.cs_x + 5, self.cs_y + 5, fill="yellow")
@@ -214,6 +262,17 @@ class CanvasWidget:
         x_loc = self.cs_x + x * self.zoom
         y_loc = self.cs_y + y * self.zoom
         self.canvas.coords(self.beacon, x_loc, y_loc, x_loc + 20, y_loc + 20)
+
+    def menubar(self, root):
+        menu = Menu(root)
+        root.config(menu=menu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Place origin beacon", command=self.add_beacon)
+        editMenu.add_command(label="Add room plan", command=self.add_floor_plan)
+        menu.add_cascade(label="Canvas...", menu=editMenu)
+
+        menu.add_command(label="Replays", command=lambda: self.root.switch_frame(ReplayFrame))
 
 
 class CanvasControls:
@@ -254,4 +313,6 @@ def main(window):
                 sys.exit()
 
 
-main(SampleApp())
+#main(SampleApp())
+
+SampleApp().mainloop()
